4c64f9baed7c1117374c8e54b737bf9629c92108
CameronCrupper <guitardude20795@yahoo.com>
2023-04-16 17:32:24 -0500

    finished for now

--- lib/screens/tables_page.dart
+++ lib/screens/tables_page.dart
@@ -16,45 +16,58 @@ class _TablesPageState extends ConsumerState<TablesPage> {
   Future<DocumentSnapshot<Map<String, dynamic>>> getUser() async {
     await Future.delayed(const Duration(seconds: 1));
     final user = FirebaseAuth.instance.currentUser;
-    final currentUser = await FirebaseFirestore.instance.collection('users')
-      .doc(user!.uid).get();
+    final currentUser = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user!.uid)
+        .get();
     return currentUser;
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getUser(),
-      builder: (context, snapshot) {
-        if (snapshot.data != null) {
-          return Column(
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const StartTablePage()
-                    )
-                  );
-                }, 
-                child: const Text('Start a new Table')
+        future: getUser(),
+        builder: (context, snapshot) {
+          if (snapshot.data != null) {
+            return Scaffold(
+              body: Container(
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Logo1.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const StartTablePage()));
+                        },
+                        child: const Text('Start a new Table')),
+                    // Expanded(
+                    //     child: ListView.builder(
+                    //         itemCount:
+                    //             snapshot.data!.data()?['tables'].length,
+                    //         itemBuilder: (context, index) {
+                    //           return Text(
+                    //               '${snapshot.data!.data()?['tables'][index]}');
+                    //         }))
+                  ],
+                ),
               ),
-              Expanded(
-                child: ListView.builder(
-                itemCount: snapshot.data!.data()?['tables'].length,
-                itemBuilder: (context, index) {
-                  return Text('${snapshot.data!.data()?['tables'][index]}');
-                })
-              )
-            ],
-          );
-        } else {
-          return  const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      }
-    );
-  } 
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
 }
