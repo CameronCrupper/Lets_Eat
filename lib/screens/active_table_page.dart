f89f527a0f03926e74566650e92fe6969f5ca771
CameronCrupper <guitardude20795@yahoo.com>
2023-04-17 16:55:55 -0500

    tables page fully done check it out

--- lib/screens/active_table_page.dart
+++ lib/screens/active_table_page.dart
@@ -23,106 +23,96 @@ class _ActiveTablePageState extends ConsumerState<ActiveTablePage> {
   Map<String, dynamic> _restaurant = {};
 
   Future<DocumentSnapshot<Map<String, dynamic>>> getTable(String table) async {
-    final tableInfo = await FirebaseFirestore.instance.collection('tables')
-      .doc(table).get();
+    final tableInfo =
+        await FirebaseFirestore.instance.collection('tables').doc(table).get();
     return tableInfo;
   }
 
-  Future<DocumentSnapshot<Map<String, dynamic>>> getFriend(String friend) async {
-    final friendInfo = await FirebaseFirestore.instance.collection('users')
-      .doc(friend).get();
+  Future<DocumentSnapshot<Map<String, dynamic>>> getFriend(
+      String friend) async {
+    final friendInfo =
+        await FirebaseFirestore.instance.collection('users').doc(friend).get();
     return friendInfo;
   }
 
   void updateAttendees() {
-    FirebaseFirestore.instance.collection('tables')
-      .doc(_tableUid)
-      .update({'attendees': _attendees});
-  }  
+    FirebaseFirestore.instance
+        .collection('tables')
+        .doc(_tableUid)
+        .update({'attendees': _attendees});
+  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getTable(widget.tableUid),
-      builder: (context, snapshot) {
-        if (snapshot.data != null) {
-          _attendees = snapshot.data!.data()!['attendees'];
-          _tableUid = snapshot.data!.data()!['uid'];
-          _tablename = snapshot.data!.data()!['tablename'];
-          _restaurant = snapshot.data!.data()!['restaurant'];
-          print(_attendees);
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.lightGreen.shade600,
-              leading: const BackButton(),
-              title: Text(
-                _tablename,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-                ),
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.ellipsis,
-              )
-            ),
-            body: Container(
-              color: Colors.white,
-              child: Column(
-                children: [
-                  if (_restaurant['name'] == 'none')
-                    ElevatedButton(
-                      onPressed: () {
-                        // Finding restaurant logic goes here
-                      },
-                      child: const Text('Find a Restaurant')
-                    ),
-                  if (_restaurant['name'] != 'none')
-                    const Text('restaurant data'),
-                  const Text('Attendees:'),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: _attendees.length,
-                      itemBuilder: (context, index) {
-                        return FutureBuilder(
-                          future: getFriend(_attendees[index]),
-                          builder: (context, snapshot) {
-                            if (snapshot.data != null) {
-                              return Row(
-                                children: [
-                                  Text('${snapshot.data!.data()!['username']}'),
-                                  const SizedBox(width: 20),
-                                  TextButton(
-                                    onPressed: () {
-                                      _attendees.remove(
-                                        snapshot.data!.data()!['uid']
-                                      );
-                                      updateAttendees();
-                                      setState(() {});
-                                    },
-                                    child: const Text('Remove From Table')
-                                  )
-                                ],
-                              );
-                            } else {
-                              return const Center(
-                                child: CircularProgressIndicator()
-                              );
-                            }
-                          }
-                        );
-                      }
-                    )
-                  ),
-                ]
-              )
-            )
-          );
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      }
-    );
+        future: getTable(widget.tableUid),
+        builder: (context, snapshot) {
+          if (snapshot.data != null) {
+            _attendees = snapshot.data!.data()!['attendees'];
+            _tableUid = snapshot.data!.data()!['uid'];
+            _tablename = snapshot.data!.data()!['tablename'];
+            _restaurant = snapshot.data!.data()!['restaurant'];
+            print(_attendees);
+            return Scaffold(
+                appBar: AppBar(
+                    backgroundColor: Colors.lightGreen.shade600,
+                    leading: const BackButton(),
+                    title: Text(
+                      _tablename,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                    )),
+                body: Container(
+                    color: Colors.white,
+                    child: Column(children: [
+                      if (_restaurant['name'] == 'none')
+                        ElevatedButton(
+                            onPressed: () {
+                              // Finding restaurant logic goes here
+                            },
+                            child: const Text('Find a Restaurant')),
+                      if (_restaurant['name'] != 'none')
+                        const Text('restaurant data'),
+                      const Text('Attendees:'),
+                      Expanded(
+                          child: ListView.builder(
+                              itemCount: _attendees.length,
+                              itemBuilder: (context, index) {
+                                return FutureBuilder(
+                                    future: getFriend(_attendees[index]),
+                                    builder: (context, snapshot) {
+                                      if (snapshot.data != null) {
+                                        return Row(
+                                          children: [
+                                            Text(
+                                                '${snapshot.data!.data()!['username']}'),
+                                            const SizedBox(width: 20),
+                                            TextButton(
+                                                onPressed: () {
+                                                  _attendees.remove(snapshot
+                                                      .data!
+                                                      .data()!['uid']);
+                                                  updateAttendees();
+                                                  setState(() {});
+                                                },
+                                                child: const Text(
+                                                    'Remove From Table'))
+                                          ],
+                                        );
+                                      } else {
+                                        return const Center(
+                                            child: CircularProgressIndicator());
+                                      }
+                                    });
+                              })),
+                    ])));
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 }
--- lib/screens/tables_page.dart
+++ lib/screens/tables_page.dart
@@ -18,17 +18,18 @@ class _TablesPageState extends ConsumerState<TablesPage> {
   late List<dynamic> tables = ref.watch(tablesProvider);
 
   Future<DocumentSnapshot<Map<String, dynamic>>> getTable(String table) async {
-    final tableInfo = await FirebaseFirestore.instance.collection('tables')
-      .doc(table).get();
+    final tableInfo =
+        await FirebaseFirestore.instance.collection('tables').doc(table).get();
     return tableInfo;
   }
 
   void updateTables() {
-    FirebaseFirestore.instance.collection('users')
-      .doc(uid)
-      .update({'tables': tables});
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .update({'tables': tables});
     ref.read(tablesProvider.notifier).updateTables(tables);
-  }  
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,15 +46,22 @@ class _TablesPageState extends ConsumerState<TablesPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            //pushing Start a New Table button down from the top
+            const SizedBox(
+              height: 350, // height you want
+            ),
             ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              const StartTablePage()));
-                },
-                child: const Text('Start a new Table')),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const StartTablePage()));
+              },
+              child: const Text('Start a New Table'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightGreen.shade600,
+              ),
+            ),
             Expanded(
               child: ListView.builder(
                 itemCount: tables.length,
@@ -64,44 +72,84 @@ class _TablesPageState extends ConsumerState<TablesPage> {
                       if (snapshot.data != null) {
                         // TILE FOR EACH TABLE IN USER'S LIST
                         return InkWell(
-                          onTap:() {
+                          onTap: () {
                             Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                  ActiveTablePage(
-                                    tableUid:snapshot.data!.data()!['uid']
-                                  )
-                              )
-                            );
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ActiveTablePage(
+                                        tableUid:
+                                            snapshot.data!.data()!['uid'])));
                           },
                           child: Container(
-                            color: Colors.white,
-                            child: Row(
+                            color: Colors.transparent,
+                            child: Column(
                               children: [
-                                Text(snapshot.data!.data()!['tablename']),
-                                const SizedBox(width: 20),
+                                //centerin tablename text
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Center(
+                                  child: Text(
+                                    snapshot.data!.data()!['tablename'],
+                                    style: const TextStyle(color: Colors.white),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                const SizedBox(height: 20, width: 20),
                                 TextButton(
                                   onPressed: () {
-                                    tables.remove(snapshot.data!.data()!['uid']);
+                                    tables
+                                        .remove(snapshot.data!.data()!['uid']);
                                     updateTables();
                                     // Enter logic for removing user from table's list of attendees
                                     setState(() {});
                                   },
-                                  child: const Text('Leave Table')
-                                )
-                              ]
+                                  //Leave table custom button
+                                  child: Container(
+                                    height: 50,
+                                    width: 150,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.shade300,
+                                      borderRadius: BorderRadius.circular(15),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.lightGreen.shade600,
+                                          spreadRadius: 1,
+                                          blurRadius: 8,
+                                          offset: const Offset(4, 4),
+                                        ),
+                                        const BoxShadow(
+                                          color: Colors.white,
+                                          spreadRadius: 2,
+                                          blurRadius: 8,
+                                          offset: Offset(-4, -4),
+                                        ),
+                                      ],
+                                    ),
+                                    child: const Center(
+                                      child: Text(
+                                        "Leave Table",
+                                        style: TextStyle(color: Colors.black),
+                                        // child: const Text(
+                                        //   'Leave Table',
+                                        //   style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          )
+                          ),
                         );
                       } else {
                         return const Center(
                           child: CircularProgressIndicator(),
                         );
                       }
-                    }
+                    },
                   );
-              })
+                },
+              ),
             ),
           ],
         ),
